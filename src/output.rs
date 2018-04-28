8bf9d0581cdaea9bc104d0ac57c7a740c83eab54
jkm <lejay@physik.tu-berlin.de>
2018-04-28 20:45:46 +0200

    create function to set up working directory

--- src/output.rs
+++ src/output.rs
@@ -1,11 +1,16 @@
-use std::io
+use std::io;
 use std::io::prelude::*;
-use std::fs::{OpenOptions};
+use std::fs;
 
+fn setupWorkFile(filepath: &str) -> Result<String, fs::Error>
+{
+    fs::create_dir_all(filepath)?;
+    Ok(filepath)
+}
 
 fn writeOutFile(line: &str, filename: &str) -> Result<String, io::Error>
 {
-    OpenOptions::new()
+    fs::OpenOptions::new()
         .append(true)
         .open(filename)?
         .writeln!(line)?;
