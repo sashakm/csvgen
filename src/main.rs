4b7482fc81907de9d01572673c2ac1c976ca4bcf
jkm <lejay@physik.tu-berlin.de>
2018-04-30 22:03:35 +0200

    setup line generator with static data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 #![crate_name = "csvgen"]
+#![feature(rand)]
+
 extern crate clap;
+extern crate rand;
+extern crate time;
 
 mod input;
 use input::Parameters;
 
+mod generator;
+use generator::CsvLine;
+
 fn main() {
     let params: Box<Parameters> = Box::new(Parameters::new().unwrap());
-
-    println!("{:?}",params)
+    let line = CsvLine::new(&params.column_types)
+                        .line_value;
+    println!("{}",line);
 }
