606aeceb73bb9b4e32e73d26bf3dc945d403aac5
jkm <lejay@physik.tu-berlin.de>
2018-05-01 04:55:27 +0200

    improve structure and clean up

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ mod output;
 use output::write_stdout;
 
 use std::thread;
+use std::sync::Arc;
 
 ///MB to Byte
 const MB: usize = 1024*1024;
@@ -24,20 +25,26 @@ const THREAD_NUM: usize = 5;
 const MAX_SIZE: usize = 100;
 
 fn main() {
-    let params: Box<Parameters> = Box::new(Parameters::new().unwrap());
+    let params: Arc<Parameters> = Arc::new(Parameters::new().unwrap());
     //Limit possible outputsize until checks for target filesystem are in place
     if &params.size > &MAX_SIZE {
         panic!("Requested output is too large.")
     }
-    for num in 1..THREAD_NUM {
+
+    fn call(col_types: &'static Vec<String>, out_limit: &'static usize) 
+        -> Result<usize, std::fmt::Error> {
         thread::spawn(move || {
             let mut output_counter: usize = 0;
-            let output_limit: usize = &params.size * &MB;
-            while &output_counter < &output_limit {
-                    let line = CsvLine::new(&params.column_types)
+            while &output_counter < &out_limit {
+                    let line = CsvLine::new(&col_types)
                                     .line_value;
                 output_counter += write_stdout(&line).unwrap();
             }
         });
+        Ok(0)
+    }
+
+    for num in 1..THREAD_NUM {
+        call(&params.column_types,&params.size);
     }
 }
