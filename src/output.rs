6495829b590780a32dfa2dc835ff3a78f608bfe2
jkm <lejay@physik.tu-berlin.de>
2018-05-01 02:06:18 +0200

    implement output functions

--- src/output.rs
+++ src/output.rs
@@ -1,19 +1,26 @@
-use std::io;
-use std::io::prelude::*;
+use std::io::{self, Write};
 use std::fs;
 
-fn setupWorkFile(filepath: &str) -> Result<String, fs::Error>
+pub fn write_stdout(line: &str) -> Result<usize, io::Error> {
+    let stdout = io::stdout();
+    let mut handle =stdout.lock();
+    let written: usize = handle.write(line.as_bytes())?;
+    Ok(written)
+}
+
+fn setup_work_file(filepath: &str) -> Result<&str, io::Error>
 {
     fs::create_dir_all(filepath)?;
     Ok(filepath)
 }
 
-fn writeOutFile(line: &str, filename: &str) -> Result<String, io::Error>
+fn append_work_file(line: &str, filename: &str) -> Result<usize, io::Error>
 {
-    fs::OpenOptions::new()
-        .append(true)
-        .open(filename)?
-        .writeln!(line)?;
+    let file = fs::OpenOptions::new()
+                                .append(true)
+                                .open(filename)?;
+    let mut handle = io::BufWriter::new(file);
+    let written: usize = handle.write(line.as_bytes())?;
 
-    Ok()
+    Ok(written)
 }
