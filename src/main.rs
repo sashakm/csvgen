6c34d0c027577d75b81bf1527b47d6cee66dd5a1
jkm <lejay@physik.tu-berlin.de>
2018-05-04 01:09:28 +0200

    remove arc for now and just clone params

--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,28 @@ mod output;
 use output::write_stdout;
 
 use std::thread;
-use std::sync::Arc;
 
 ///MB to Byte
 const MB: usize = 1024*1024;
-///Threading threshold
-const THREAD_NUM: usize = 5;
+///Max worker threads
+const THREAD_NUM: usize = 2;
 ///Max size of output-file
 const MAX_SIZE: usize = 100;
 
 fn main() {
-    let params: Arc<Parameters> = Arc::new(Parameters::new().unwrap());
+    let params: Parameters = Parameters::new().unwrap();
     //Limit possible outputsize until checks for target filesystem are in place
     if &params.size > &MAX_SIZE {
         panic!("Requested output is too large.")
     }
 
-    fn call(col_types: &'static Vec<String>, out_limit: &'static usize) 
-        -> Result<usize, std::fmt::Error> {
+    fn call(params: Parameters)-> thread::Result<usize> {
         thread::spawn(move || {
+            println!("thread spawned");
             let mut output_counter: usize = 0;
-            while &output_counter < &out_limit {
-                    let line = CsvLine::new(&col_types)
+            let size_in_byte = (&params.size*&MB) / &THREAD_NUM;
+            while &output_counter < &size_in_byte {
+                    let line = CsvLine::new(&params.column_types)
                                     .line_value;
                 output_counter += write_stdout(&line).unwrap();
             }
@@ -44,7 +44,9 @@ fn main() {
         Ok(0)
     }
 
-    for num in 1..THREAD_NUM {
-        call(&params.column_types,&params.size);
+    for num in 1..THREAD_NUM+1 {
+        println!("Thread init");
+        let p = params.clone();
+        call(p);
     }
 }
