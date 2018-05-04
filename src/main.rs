3934176e8b1d1ab3564594e30f7ce856b66d4c27
jkm <lejay@physik.tu-berlin.de>
2018-05-04 02:35:14 +0200

    move thread closure to main and join threads in second loop

--- src/main.rs
+++ src/main.rs
@@ -30,23 +30,23 @@ fn main() {
         panic!("Requested output is too large.")
     }
 
-    fn call(params: Parameters)-> thread::Result<usize> {
-        thread::spawn(move || {
-            println!("thread spawned");
+    let mut workers: Vec<thread::JoinHandle<()>> = Vec::with_capacity(THREAD_NUM);
+
+    for num in 1..THREAD_NUM+1 {
+        let p = params.clone();
+        let worker = thread::spawn(move || {
             let mut output_counter: usize = 0;
-            let size_in_byte = (&params.size*&MB) / &THREAD_NUM;
+            let size_in_byte = (&p.size*&MB) / &THREAD_NUM;
             while &output_counter < &size_in_byte {
-                    let line = CsvLine::new(&params.column_types)
+                let line = CsvLine::new(&p.column_types)
                                     .line_value;
                 output_counter += write_stdout(&line).unwrap();
             }
         });
-        Ok(0)
+        workers.push(worker);
     }
 
-    for num in 1..THREAD_NUM+1 {
-        println!("Thread init");
-        let p = params.clone();
-        call(p);
+    for w in workers {
+        w.join().unwrap();
     }
 }
