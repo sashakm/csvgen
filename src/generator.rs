c6283df4436631fd8a8179f89cc26b54827562f9
jkm <lejay@physik.tu-berlin.de>
2018-05-08 19:13:27 +0200

    update inline doc

--- src/generator.rs
+++ src/generator.rs
@@ -3,22 +3,30 @@ const DEFAULT_SCHTRING: &str = "test";
 const DEFAULT_INT: usize = 42;
 
 enum CellType {
+    /// Supported Types for column-cells
     Float,
     Schtring,
     Int
 }
 
 struct Cell {
+    /// Represents one cell in a row
     cell_value: String,
 }
 
 pub struct CsvLine {
+    /// Represents one row of csv consisting of multiple Cells with CellTypes
     pub line_value: String
 }
 
 impl Cell {
+    /// Populate cell according to given CellType
+    /// ```
+    /// use csvgen::generator::{Cell,CellType};
+    /// let c_type = CellType::Int;
+    /// let c = Cell::new(c_type);
+    /// ```
     pub fn new(c_type: CellType) -> Cell {
- 
         Cell {
             cell_value: match c_type {
                 CellType::Float => DEFAULT_FLOAT.to_string(),
@@ -26,10 +34,18 @@ impl Cell {
                 CellType::Int => DEFAULT_INT.to_string(),
             }
         }
-    }
+    } 
 }
 
 impl CsvLine {
+    /// Create a csv-row according to given types.
+    /// For each type, a Cell will be generated. The Cell-value 
+    /// is then pushed onto a String held by CsvLine.
+    /// ```
+    /// use csvgen::generator::CsvLine;
+    /// let line_types = Vec::from(vec!["int","string"]);
+    /// let line = CsvLine::new(&line_types);
+    /// ```
     pub fn new(l_types: &Vec<String>) -> CsvLine {
         let mut val: String = String::new();
         for (cell_num, lt) in l_types.iter().enumerate() {
